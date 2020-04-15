3c182e707d59812f925e739fc7b8e1673e86de23
Jacek Góraj <jckgrj@gmail.com>
2020-04-15 12:35:41 +0200

    edited links so that they are shown for every resource

--- src/main/java/com/goraj/jacek/springbootsandbox/restexample/PlayerController.java
+++ src/main/java/com/goraj/jacek/springbootsandbox/restexample/PlayerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @RestController
 public class PlayerController {
@@ -20,8 +21,12 @@ public class PlayerController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping(path = "/players")
-    public CollectionModel<List<Player>> getPlayers() {
-        List<Player> players = repository.findAll();
+    public CollectionModel<EntityModel<Player>> getPlayers() {
+        List<EntityModel<Player>> players = repository.findAll().stream()
+                .map(player -> new EntityModel<>(player,
+                        linkTo(methodOn(PlayerController.class).getPlayers()).withSelfRel(),
+                        linkTo(methodOn(PlayerController.class).getPlayers()).withRel("players")))
+                .collect(Collectors.toList());
         return new CollectionModel(players,
                 linkTo(methodOn(PlayerController.class).getPlayers()).withSelfRel());
     }
